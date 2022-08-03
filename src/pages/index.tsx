e6242a701cb1b2c20b589fba8d32bf8f74739924
Gaishi Kudo <40158437+ihsiag@users.noreply.github.com>
2022-08-03 17:48:31 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,41 @@
-import type {NextPage } from "next";
+import type { NextPage } from "next";
 import Link from "next/link";
 import Text from "components/atoms/Text";
+import Box from "components/layout/Box";
 import Flex from "components/layout/Flex";
 import Layout from "components/templates/Layout";
 import { useAuthGaurd } from "utils/hooks";
+import styled from "styled-components";
 
+const Anchor = styled(Text)`
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const HomePage: NextPage = () => {
-    useAuthGaurd();
-    return (
+  useAuthGaurd();
+  return (
     <Layout>
-      <Flex padding={2} justifyContent="center" backgroundColor="primary">
-        <Text
+      <Flex width ="100%" height="100%" padding={4} justifyContent="center" alignItems="center">
+        {/* <Text
           as="h1"
           marginBottom={0}
           color="black"
           variant="extraLarge"
         >
-          マイページ
-        </Text>
+          サインインする
+        </Text> */}
+        <Flex
+          justifyContent={"center"}
+        >
+          <Link href="/signin">
+            <Anchor>
+              <Text as="h1">サインインする</Text>
+            </Anchor>
+          </Link>
+        </Flex>
       </Flex>
     </Layout>
   );
